a4ee0fa3922f8d6bbfc194f4e7762fad271e2d6e
Gao Mingfei <mingfei.gao@ucloud.cn>
2020-04-19 17:07:13 +0800

    Add some test.
    Signed-off-by: Gao Mingfei <mingfei.gao@ucloud.cn>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
+#include <cinatra.hpp>
 #include <iostream>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  uint32_t max_thread_num = std::thread::hardware_concurrency();
+  cinatra::http_server server(max_thread_num);
+  server.listen("0.0.0.0", "80");
+  server.set_http_handler<cinatra::GET, cinatra::POST>(
+      "/", [](cinatra::request &req, cinatra::response &res) {
+        res.set_status_and_content(cinatra::status_type::ok, "hello world");
+      });
+
+  server.run();
   return 0;
 }
